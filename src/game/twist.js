7fd4adfda90b28569c6ee73c1c3ec874121c4685
Marko Peric <mperic@egamingc.com>
2022-04-12 01:59:39 +0200

    twist refactor

--- src/game/twist.js
+++ src/game/twist.js
@@ -1,22 +1,39 @@
 import {app, pc} from "../initApp";
 
-var plateCount = 16;
-var plates = [];
-var spiral = true;
+let plateCount = 16;
+let plates = [];
+let spiral = true;
 
-function createShape(type, x, y, z, angle = 0, length = 1) {
-  var shape = new pc.Entity();
-  shape.addComponent('model', { type: type });
-  app.root.addChild(shape);
-  shape.model.castShadows = true;
-  var material = shape.model.model.meshInstances[0].material;
-  material.diffuse.set(1, 1, 1);
+const container = new pc.Entity();
+
+function createMaterial(color) {
+  const material = new pc.StandardMaterial();
+  material.diffuse = color;
   material.specular.set(0.4, 0.4, 0.4);
   material.shininess = 70;
   material.update();
-  if(type === 'box')shape.setLocalScale(1, 0.2, length);
-  if(type === 'sphere')shape.setLocalScale(0.2, 0.2, 0.2);
+  return material;
+}
+const gray = createMaterial(new pc.Color(1, 1, 1));
+const red = createMaterial(new pc.Color(1, 0.3, 0.3));
+
+function createBallShape(x, y, z) {
+  const shape = new pc.Entity();
+  shape.addComponent('model', { type: 'sphere', material: red });
+  app.root.addChild(shape);
+  shape.model.castShadows = true;
+  shape.setLocalScale(0.2, 0.2, 0.2);
   shape.setPosition(x, y, z);
+  return shape;
+}
+function createPlateShape(x, y, z, angle = 0, length = 1) {
+  const shape = new pc.Entity();
+  shape.addComponent('model', { type: 'box' , material: gray });
+  //app.root.addChild(shape);
+  container.addChild(shape);
+  shape.setLocalScale(1, 0.2, length);
+  //shape.setPosition(x, y, z);
+  shape.setLocalPosition(x, y, z);
   shape.rotate(0, 0, angle);
   return shape;
 }
@@ -25,83 +42,71 @@ export default() => {
   app.start();
 
   // Create camera entity
-  var camera = new pc.Entity();
+  let camera = new pc.Entity();
   camera.addComponent('camera', {
     clearColor: new pc.Color(0.1, 0.2, 0.3)
   });
+  camera.setPosition(0, 0, 6);
+  app.root.addChild(camera);
 
   // Create directional light entity
-  var light = new pc.Entity();
-  light.addComponent('light');
+  const dLight = { type: "directional", color: new pc.Color(1, 1, 1) }
+  const light0 = new pc.Entity();
+  light0.addComponent("light", dLight);
+  light0.setLocalEulerAngles(45, 30, 0);
+  app.root.addChild(light0);
+  const light1 = new pc.Entity();
+  light1.addComponent("light", dLight);
+  light1.setLocalEulerAngles(45, 30, 180);
+  app.root.addChild(light1);
 
-  // Add to hierarchy
-  app.root.addChild(camera);
-  app.root.addChild(light);
-
-  // Set up initial positions and orientations
-  camera.setPosition(0, 0, 6);
-  //camera.rotate(0, 0, 45);
-  //light.rotate(0, 0, 45);
-  light.setEulerAngles(45, 0, 0);
-
-  // Create a rotation script
-  var Rotate = pc.createScript('rotate');
-  Rotate.prototype.update = function (dt) {
-    this.entity.rotate(10 * dt, 20 * dt, 30 * dt);
-  };
-  // Create a movement script
-  var Fly = pc.createScript('fly');
+  // Create a movement script (used for plates)
+  let Fly = pc.createScript('fly');
   Fly.prototype.update = function (dt) {
-    var pos = this.entity.getPosition();
+    let pos = this.entity.getPosition();
     if(pos.z<4) {
       this.entity.setPosition(pos.x, pos.y, pos.z + 0.5 * dt);
+      //this.entity.setLocalPosition(pos.x, pos.y, pos.z + 0.5 * dt);
     }else{
       this.entity.setPosition(pos.x, pos.y, pos.z - plates.length );
+      //this.entity.setLocalPosition(pos.x, pos.y, pos.z - plates.length );
     }
   };
-  // var timer = 0;
-  // // Create a bounce script
-  // var Bounce = pc.createScript('bounce');
-  // Bounce.prototype.update = function (dt) {
-  //   timer += dt;
-  //     //var pos = this.entity.getPosition();
-  //     //this.entity.setPosition(pos.x, pos.y, pos.z + 0.2 * dt);
-  //    this.entity.setPosition(0, Math.sin(timer*2), 0);
-  // };
 
-  // Create box entity
-  var cube = new pc.Entity();
-  cube.addComponent('model', {type: "box"});
-  //cube.rotate(10, 15, 45);
-  cube.setLocalScale(1, 1, 1);
-  //cube.translate(-2, 0, 0);
-  // Add rotation script to cube
-  //cube.addComponent('script');
-  //cube.script.create('rotate');
-  //cube.script.create('fly');
-  //app.root.addChild(cube);
+  container.name = 'container';
+  app.root.addChild(container);
+  //container.setPosition(2,0,0);
+  //container.rotate(0,0,45);
 
-  var ball = createShape('sphere', 0,-1.21,1);
-  //var pos = ball.getPosition();
-  // gsap.to(pos,{duration: 0.5, ease: "bounce.easeOut", y: 0.5, yoyo:true, repeat:-1, onUpdate: function() {
-  //     ball.setPosition(pos);
-  //   }});
+  // // tween version //
+  // container.tween(container.getLocalEulerAngles())
+  //     .rotate(new pc.Vec3(0, 0, 45), 0.5, pc.Linear)
+  //     //.to(new pc.Vec3(0, 0.2, 0), 0.5, pc.SineOut)
+  //     .loop(true)
+  //     .yoyo(true)
+  //     //.repeat(2)
+  //     .start();
 
-  // ball.addComponent('script');
-  // ball.script.create('bounce');
+  let ball = createBallShape( 0,-1.21,1);
 
-  //    cone.setPosition(2, Math.sin(timer*2), 0);
+  // // tween version //
+  // ball.tween(ball.getLocalPosition())
+  //     .to(new pc.Vec3(0, 0.2, 0), 0.5, pc.SineOut)
+  //     //.loop(true)
+  //     .yoyo(true)
+  //     .repeat(2)
+  //     .start();
 
-  var x = 0;
-  var y = -1.4;
-  var z = 0;
-  var a = 0;
-  var l = 1;
-  var pl = 0;
+  let x = 0;
+  let y = -1.4;
+  let z = 0;
+  let a = 0;
+  let l = 1;
+  let pl = 0;
   //initial tile
-  plates[0] = createShape('box', x, y, z, a, l);
+  plates[0] = createPlateShape( x, y, z, a, l);
   //other tiles
-  for (var i = 1; i < plateCount; i++){
+  for (let i = 1; i < plateCount; i++){
     if(!spiral && Math.random() < 0.5){
       // console.log('left');
       a = a - 45;
@@ -131,9 +136,9 @@ export default() => {
     //z = pl === 1 ? -1 * i : -1 * i - pl/2; // - (pl - 1);
     // console.log(plates[i-1].getPosition());
     //console.log(plates[i-1].getLocalScale());
-    plates[i] = createShape('box', x, y, -1 * i, a, l);
+    plates[i] = createPlateShape( x, y, -1 * i, a, l);
   }
-  for (var i = 0; i < plates.length; i++){
+  for (let i = 0; i < plates.length; i++){
     plates[i].addComponent('script');
     plates[i].script.create('fly');
   }
